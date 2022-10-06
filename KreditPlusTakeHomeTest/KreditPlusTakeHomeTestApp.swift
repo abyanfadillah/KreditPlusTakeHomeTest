f2c6a632eb6e2c29598ebde61f46b2267748ffda
abyan <abyanfadillah12@gmail.com>
2022-10-07 00:41:11 +0700

    Initial Commit

---
+++ KreditPlusTakeHomeTest/KreditPlusTakeHomeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  KreditPlusTakeHomeTestApp.swift
+//  KreditPlusTakeHomeTest
+//
+//  Created by Muhammad Abyan Fadillah on 07/10/22.
+//
+
+import SwiftUI
+
+@main
+struct KreditPlusTakeHomeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
